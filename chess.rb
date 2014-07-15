e152551f62d4f62d320a62a60d3ffe5c3dea1d20
Andrew & Andrew <andrew+andrew@appacademy.io>
2014-07-15 14:57:25 -0400

    Complete, removed employee

--- chess.rb
+++ chess.rb
@@ -14,6 +14,10 @@ class Piece
   def symbol
   end
 
+  def valid_moves
+    moves.select { |pos| !self.move_into_check?(pos) }
+  end
+
   def move_into_check?(pos)
     @board.dup.move(@position, pos).in_check?(@color)
   end
@@ -28,14 +32,14 @@ class SlidingPiece < Piece
     arr = []
     move_dirs.each do |delta|
       tmp = [self.position[0] + delta[0], self.position[1] + delta[1]]
-      until tmp.any? { |n| n < 0 || n > 7 } ||
-        (@board[tmp] && @board[tmp].color == self.color)
-        arr << tmp
+      until tmp.any? { |n| n < 0 || n > 7 } || @board[tmp]
+        arr << tmp.dup
         tmp[0] += delta[0]
         tmp[1] += delta[1]
       end
+      arr << tmp.dup if @board[tmp] && @board[tmp].color != @color
     end
-    arr.select { |pos| !self.move_into_check?(pos) }
+    arr
   end
 end
 
@@ -49,14 +53,15 @@ class SteppingPiece < Piece
       tmp = [self.position[0] + delta[0], self.position[1] + delta[1]]
       unless tmp.any? { |n| n < 0 || n > 7 } ||
         (@board[tmp] && @board[tmp].color == self.color)
-        arr << tmp
+        arr << tmp.dup
       end
     end
-    arr.select { |pos| !self.move_into_check?(pos) }
+    arr
   end
 end
 
 class Pawn < Piece
+  attr_accessor :has_moved
   def initialize(position, color, board)
     @has_moved = false
     @direction = color == :black ? 1 : -1
@@ -73,9 +78,7 @@ class Pawn < Piece
     arr << tmp.dup if @board[tmp] && @board[tmp].color != self.color
     tmp[1] -= 2
     arr << tmp if @board[tmp] && @board[tmp].color != self.color
-
-
-    arr.select { |pos| !move_into_check?(pos) }
+    arr
   end
 
   def symbol
@@ -173,8 +176,6 @@ class King < SteppingPiece
   end
 end
 
-$count = 0
-
 class Board
   TYPES = [
     Rook,
@@ -188,7 +189,6 @@ class Board
   ]
 
   def initialize
-    puts "#{$count += 1}"
     @grid = Array.new(8) { Array.new(8) }
 
     i = 0
@@ -208,14 +208,14 @@ class Board
         return true if piece.moves.include?(king_pos)
       end
     end
+    false
   end
   def checkmate?(color)
     king_pos = @grid.flatten.select { |piece|
       piece.is_a?(King) && piece.color == color }[0].position
-
     @grid.flatten.each do |piece|
       if piece && piece.color == color
-        return false if !piece.moves.empty?
+        return false if !piece.valid_moves.empty?
       end
     end
     true
@@ -230,13 +230,17 @@ class Board
   def move(start, end_pos)
     raise "No piece found at #{start}" if self[start].nil?
     raise "Invalid movement" if !self[start].moves.include?(end_pos)
+    old_val = self[end_pos]
     self[end_pos] = self[start]
     self[end_pos].position = end_pos
     self[start] = nil
     self
   end
   def display
+    i = -1
+    puts "+ 0 1 2 3 4 5 6 7"
     @grid.each do |row|
+      print "#{i += 1} "
       row.each do |piece|
         print piece ? piece.symbol + ' ' : '_ '
       end
@@ -263,23 +267,38 @@ class Game
   end
 
   def play
+    turns = 1
     color = :white
     while !@board.checkmate?(color)
       @board.display
       begin
         piece_pos = @players[color].get_piece_pos(color).reverse
+        raise "Cannot select opponent's piece" if
+          @board[piece_pos] &&
+          @board[piece_pos].color != color
         str = @board[piece_pos].class
         puts "#{str}"
         move = @players[color].get_move(color).reverse
+        old_board = @board.dup
         @board.move(piece_pos, move)
+        if @board.in_check?(color)
+          @board = old_board
+          raise "Move would result in a check"
+        elsif @board[move].is_a?(Pawn)
+          @board[move].has_moved = true
+        end
       rescue StandardError => error
         puts error.message
+        puts error.backtrace
         retry
       end
+      puts "Check!" if @board.in_check?(color == :white ? :black : :white)
       color = color == :white ? :black : :white
+      turns += 1
     end
-
     @board.display
+    color = color == :white ? :black : :white
+    puts "Checkmate! Winner: #{color} in #{turns/2} moves"
 
   end
 
